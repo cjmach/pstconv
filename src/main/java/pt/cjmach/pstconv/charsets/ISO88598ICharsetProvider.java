64a048c9be4782050a088c6bcceeefb1b1e999c6
Carlos Machado <58347209+cjmach@users.noreply.github.com>
2024-11-02 20:46:39 +0000

    Add support for UTF-7 and ISO-8859-8-I encodings.

---
+++ src/main/java/pt/cjmach/pstconv/charsets/ISO88598ICharsetProvider.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright 2024 Carlos Machado
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package pt.cjmach.pstconv.charsets;
+
+import java.nio.charset.Charset;
+import java.nio.charset.spi.CharsetProvider;
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ *
+ * @author cmachado
+ */
+public class ISO88598ICharsetProvider extends CharsetProvider {
+    private static final String CHARSET_NAME = "ISO-8859-8-I";
+    
+    private final Charset charset = Charset.forName("ISO-8859-8");
+
+    @Override
+    public Iterator<Charset> charsets() {
+        HashSet<Charset> set = new HashSet<>(1);
+        set.add(charset);
+	return set.iterator();
+    }
+
+    @Override
+    public Charset charsetForName(String charsetName) {
+        if (CHARSET_NAME.equalsIgnoreCase(charsetName)) {
+            return charset;
+        }
+        return null;
+    }
+    
+}
